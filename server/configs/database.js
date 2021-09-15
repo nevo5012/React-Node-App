1f1a5b56f5870ff8c7b47a4f32528a4828015110
Roi <roi@morphisec.com>
2021-09-15 13:22:01 +0300

    Update server project

--- server/configs/database.js
+++ server/configs/database.js
@@ -3,8 +3,7 @@ import mongoose, { Connection } from 'mongoose'
 const DB_CONNECTION = process.env.CONNECTION_STRING || 'mongodb://localhost:27017/deliveryDB';
 mongoose.connect(DB_CONNECTION, { 
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
+    useUnifiedTopology: true
 });
 
  
--- server/main.js
+++ server/main.js
@@ -2,12 +2,8 @@ import express from 'express'
 import cors from 'cors'
 import './configs/database.js'
 
-
-import membersController from './members/memberController.js'
 import ordersController from './orders/orderController.js'
-import userController from './users/userController.js'
 
-require("dotenv").config();
 let app = express()
 const port = process.env.PORT || 8000;
 
@@ -18,9 +14,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use('/health',(req, res) => {
     res.status(200).send('Ok');
   });
-app.use('/api/members', membersController);
 app.use('/api/orders', ordersController);
-app.use('/api/users', userController);
 
 app.listen(port);
 console.log('Server is up on localhost, listening to port 8000');
